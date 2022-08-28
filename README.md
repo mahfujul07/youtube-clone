96bc0f8a4fdde733906afe9a9e6c3575631d701b
mahfujul07 <mahfujulhuq07@gmail.com>
2022-08-28 21:05:00 +0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+### Youtube Clone React Js
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Paper, IconButton } from '@mui/meterial';
+import { SearchIcon } from '@mui/icons';
+
+const searchBar = () => {
+  return (
+    <div>searchBar</div>
+  )
+}
+
+export default searchBar
